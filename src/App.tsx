5001faa51c57a2cf70609a07aad5a2857bacca7c
Dale B <daleban.dev@gmail.com>
2023-08-18 23:18:49 +0800

    chore: setup basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import rainbow from "./assets/rainbow.svg";
+import ioLogo from "./assets/google-io-logo.svg";
+
 function App() {
   return (
-    <div className="text-3xl font-bold text-center mt-10">
-      Google I/O Extended Bacolod 2023
-    </div>
+    <section className="bg-[#202124] text-white h-screen grid place-items-center">
+      <div className="border-2 border-[#FEFEFE] rounded-2xl overflow-hidden">
+        <div>
+          <img src={ioLogo} alt="Google I/O Logo" />
+          <h1 className="font-medium text-6xl">Extended Bacolod 2023</h1>
+          <div>
+            <input type="text" />
+            <button>Get Certificate</button>
+          </div>
+        </div>
+        <img src={rainbow} alt="Rainbow" className="scale-105" />
+      </div>
+    </section>
   );
 }
 
