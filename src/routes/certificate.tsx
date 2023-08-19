05b939a6d4c7ccfe89a0a97b3345151802dda93e
Josh Daniel <joshxfi.dev@gmail.com>
2023-08-19 13:37:19 +0800

    feat: setup firebase

--- src/routes/certificate.tsx
+++ src/routes/certificate.tsx
@@ -5,7 +5,7 @@ export const Certificate = () => {
     <section className="bg-[#202124] min-h-screen text-white grid place-items-center">
       <Tilt gyroscope className="max-w-[900px] mx-auto">
         <div className="w-full relative rounded-lg grid place-items-center overflow-hidden">
-          <p className="absolute font-google uppercase left-24">
+          <p className="absolute font-google-reg uppercase md:left-24">
             Josh Daniel Bañares
           </p>
           <img
