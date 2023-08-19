0251af741e58eb60dc2df3e397aae27615012b1c
Josh Daniel <joshxfi.dev@gmail.com>
2023-08-19 13:10:35 +0800

    feat: display parallax certificate

--- src/routes/certificate.tsx
+++ src/routes/certificate.tsx
@@ -1,7 +1,20 @@
+import Tilt from "react-parallax-tilt";
+
 export const Certificate = () => {
   return (
-    <section>
-      <h1>Hello World</h1>
+    <section className="bg-[#202124] min-h-screen text-white grid place-items-center">
+      <Tilt gyroscope className="max-w-[900px] mx-auto">
+        <div className="w-full relative rounded-lg grid place-items-center overflow-hidden">
+          <p className="absolute font-google uppercase left-24">
+            Josh Daniel Bañares
+          </p>
+          <img
+            className="w-full pointer-events-none h-full object-cover"
+            src="/images/participation.png"
+            alt="PowerOn Certificate"
+          />
+        </div>
+      </Tilt>
     </section>
   );
 };
